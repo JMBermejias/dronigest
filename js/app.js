ec992989625c35c7a94aebab5cf8f46fb86a53de
JMBermejias <jmbermejias@gmail.com>
2026-09-04 18:49:58 +0200

    chore: actualizar versiones a 1.5.4 en manifest, backend, service worker y PDF

--- js/app.js
+++ js/app.js
@@ -3284,7 +3284,7 @@ Dronigest.Informes = {
     <div class="report-section-title">Datos del informe</div>
     <div class="report-info"><table>${rows}</table></div>
     <div class="report-footer">
-        Informe generado el ${fechaStr} &mdash; Dronigest v1.5.3<br>
+        Informe generado el ${fechaStr} &mdash; Dronigest v1.5.4<br>
         Este documento es un resumen informativo sin validez legal.
     </div>
     <script>
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dronigest-v9';
+const CACHE_NAME = 'dronigest-v10';
 const STATIC_ASSETS = [
     './',
     './index.html',
